1649778c4ae9eb69892321b0e35d304d7400f0f8
zhaofulin <zhaofulin@apexsoft.com.cn>
2019-07-17 11:44:27 +0800

    0303—_数据渲染

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: 'Tom',
+    age: 18,
+    students: [
+      { id: 110, name: 'kobe',age: 18 },
+      { id: 111, name: 'james', age: 32 },
+      { id: 112, name: 'curry', age: 30 },
+      { id: 113, name: 'why', age: 36 },
+    ],
+    counter:0
+  },
 
+  handleBtnClick(){
+
+    //错误的做法 界面不会刷新
+    // this.data.counter += 1
+
+    //2.this.setData();
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+
+  handleSubTraction(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
 
   /**
