3953416ecfdb8b6c3d38082bebddd14837fee436
zhaofulin <zhaofulin@apexsoft.com.cn>
2019-07-18 10:52:59 +0800

    0505_注册page和数据调用

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,93 @@
+
+//getApp()获取APP产生的示例对象
+const app = getApp()
+const name = app.globalData.name;
+const age = app.globalData.age;
+console.log(name,age)
+
 // pages/home/home.js
 Page({
-
+  //----------1.监听数据初始 --------------
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:[]
   },
-
+  //----------2.监听生命周期函数 --------------
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
+    console.log('onLoad');
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success: (res) => {
+        console.log(res);
+        const data = res.data.data.list;
+        this.setData({
+          list: data
+        })
+      }
+    })
+  },
 
+  /**
+  * 生命周期函数--监听页面显示
+  */
+  onShow() {
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady() {
+    console.log('onReady')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide() {
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
+  onUnload() {
+    console.log('onUnload')
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
+  //----------3.监听wxml 页面中相关的一些事件 --------------
+  handleGetUserInfo(event) {
+    console.log('111111111')
+    console.log(event)
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  //----------4.监听其他事件 --------------
 
+  //滚动事件
+  onPageScroll(obj){
+    console.log(obj)
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  //页面上拉触底事件的处理函数
+  onReachBottom() {
+    console.log('页面滚动到底部')
+  },
 
+  //页面相关事件处理函数--监听用户下拉动作
+  onPullDownRefresh() {
+    console.log('下拉动作')
+  },
+
+ //用户点击右上角分享
+  onShareAppMessage() {
+    console.log('onShareAppMessage')
   }
+
 })
