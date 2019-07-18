81d1f7589f878048158d9ade2304ba1274df9344
zhaofulin <zhaofulin@apexsoft.com.cn>
2019-07-18 11:02:57 +0800

    初始化界面

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 //app.js
 App({
+   //小程序初始化完成，会执行的生命周期函数
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
+
+  //小程序界面显示完成后执行的生命周期函数
+  onShow: function (options){
+    //判断小程序进入场景
+    switch (options.scene){
+      case 1001:
+        break;
+      case 1025:
+        break;
+    }
+
+    //获取用户信息，传递用户信息
+    wx.getUserInfo({
+      success: function(res){
+      }
+    })
+    
+  },
+
+  //小程序关闭，会执行的生命周期函数
+  onHide: function(){
+    
+  },
+
+  //小程序发生错误，会执行的生命周期函数
+  onError: function(){
+    console.log('发生错误: onError');
+  },
+
+  //全景数据
   globalData: {
-    userInfo: null
+    name: 'zhangsan',
+    age: 18
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,4 @@
+
 // pages/home/home.js
 Page({
 
@@ -5,62 +6,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
+    console.log('onLoad')
+  },
 
+  /**
+  * 生命周期函数--监听页面显示
+  */
+  onShow() {
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady() {
+    console.log('onReady')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide() {
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onUnload() {
+    console.log('onUnload')
   }
 })
