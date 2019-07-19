a80ffe45822f92384600aef073cfb37cb7c0ed92
zhaofulin <zhaofulin@apexsoft.com.cn>
2019-07-19 15:30:33 +0800

    WXML语法解析

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,42 +6,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:[]
+    message: '你好小程序',
+    firstname: 'kobe',
+    lastname: 'bryant',
+    age: 12,
+    nowTime: new Date().toLocaleString(),
+    isActive: false,
+    isShow: true,
+    scrore: 45,
+    moives: ['扫毒','星际穿越','流浪地球'],
+    moiveNum: [
+      ['111', '222', '333'],
+      ['扫毒', '星际穿越', '流浪地球'],
+      ['aaa', 'bbb', 'ccc']
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    console.log('onLoad')
+  onLoad() {
+    setInterval(() => {
+      this.setData({
+        nowTime: new Date().toLocaleString()
+      })
+    },1000)
   },
 
-  /**
-  * 生命周期函数--监听页面显示
-  */
-  onShow() {
-    console.log('onShow')
+  handleColor(){
+    this.setData({
+      isActive: !this.data.isActive
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-    console.log('onReady')
+  handleShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
   },
 
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-    console.log('onHide')
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-    console.log('onUnload')
+  handleAdd() {
+    this.setData({
+      scrore: this.data.scrore + 5
+    })
   }
+
+  
 })
