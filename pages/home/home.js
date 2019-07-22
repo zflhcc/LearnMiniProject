4bbf021cf9a2a3618ac0bf82f96c1563de35d8b1
zhaofulin <zhaofulin@apexsoft.com.cn>
2019-07-22 10:32:08 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,42 +6,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:[]
+    title:['衣服','裤子','鞋子'],
+    currentIndex: 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    console.log('onLoad')
+  handleTouchStart(){
+    console.log('handleTouchStart')
   },
 
-  /**
-  * 生命周期函数--监听页面显示
-  */
-  onShow() {
-    console.log('onShow')
+  handleTouchMove() {
+    console.log('handleTouchMove')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-    console.log('onReady')
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
   },
 
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-    console.log('onHide')
+  handleLongPress() {
+    console.log('handleLongPress')
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-    console.log('onUnload')
+  handleTap() {
+    console.log('handleTap')
+  },
+
+  handleEventClick(event){
+    console.log(event)
+  },
+  handleInner(event){
+    console.log(event)
+  },
+  handleOuter(event) {
+    console.log(event)
+  },
+  handleItem(event){
+    console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index;
+    const title = dataset.title;
+
+    this.setData({
+      currentIndex:index
+    })
+  },
+  handleCaptureView1(){
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   }
+
 })
